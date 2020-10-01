5c4b756b0a4c9ab994ddfba2de8e9a607161cee6
Tony Wang <yangwang@wisers.com>
2020-10-01 11:13:12 +0800

    [mod] implement daily drinks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,59 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
+import OrderItem from "./components/OrderItem";
+import * as OrderItemConstants from "./components/OrderItem/constants";
+import { ReactComponent as AddIcon } from "./components/Icons/add.svg";
 import "./App.css";
 
 function App() {
+  const [orderList, setOrderList] = useState([]);
+  const onEditOrder = useCallback(
+    (index) => (columnType, value) => {
+      const newOrderList = orderList.slice();
+      newOrderList[index][columnType] = value;
+      setOrderList(newOrderList);
+    },
+    [orderList, setOrderList]
+  );
+  const onDelete = useCallback(
+    (index) => () => {
+      const newOrderList = orderList.slice();
+      newOrderList.splice(index, 1);
+      setOrderList(newOrderList);
+    },
+    [orderList, setOrderList]
+  );
+  const onAddItem = useCallback(() => {
+    const newOrderList = orderList.slice();
+    newOrderList.push({
+      [OrderItemConstants.NAME]: "",
+      [OrderItemConstants.PRICE]: "",
+      [OrderItemConstants.COMMENT]: "",
+    });
+    setOrderList(newOrderList);
+  }, [orderList, setOrderList]);
+
   return (
     <div className="App">
-      <header className="App-header">test</header>
+      <div className="App-title">Daily Drinks</div>
+      <div className="App-content" data-testid="app_content">
+        {orderList.map((item, index) => (
+          <OrderItem
+            className="App-orderItem"
+            data-testid="app_orderItem"
+            key={index}
+            info={item}
+            onChange={onEditOrder(index)}
+            onDelete={onDelete(index)}
+          />
+        ))}
+        <div className="App-add-orderItem">
+          <AddIcon
+            className="App-addIcon"
+            data-testid="app_add_orderItem"
+            onClick={onAddItem}
+          />
+        </div>
+      </div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders Daily Drinks", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = getByText(/Daily Drinks/i);
+  expect(titleElement).toBeInTheDocument();
+});
+
+test("renders content", () => {
+  const { queryByTestId } = render(<App />);
+  const contentElement = queryByTestId("app_content");
+  expect(contentElement).toBeInTheDocument();
+});
+
+test("renders add button area", () => {
+  const { queryByTestId } = render(<App />);
+  const addElement = queryByTestId("app_add_orderItem");
+  expect(addElement).toBeInTheDocument();
 });
